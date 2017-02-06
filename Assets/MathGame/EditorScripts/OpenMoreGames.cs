264cad28e602f2d1439901349c05e40611187b61
trunghieu11 <vuondenthanhcong11@gmail.com>
2017-02-06 22:47:26 +0700

    Initial project

---
+++ Assets/MathGame/EditorScripts/OpenMoreGames.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace AppAdvisory.MathGame
+{
+	public class OpenMoreGames : MonoBehaviour 
+	{
+		public void OnClicked()
+		{
+			Application.OpenURL("http://u3d.as/9cs");
+		}
+	}
+}
